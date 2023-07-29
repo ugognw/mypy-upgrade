da975db1001c90e517f4cfce78b1093c2fb297a3
Ugochukwu Nwosu <ugognw@gmail.com>
2023-07-29 03:37:57 -0600

    Add tests for get_module_paths
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

---
+++ tests/test_get_module_paths.py
@@ -0,0 +1,12 @@
+import pathlib
+
+from mypy_upgrade.__main__ import get_module_paths
+
+
+def test_should_return_path_of_testfile():
+    path = get_module_paths([__name__])[0]
+    assert path == pathlib.Path(__file__).resolve()
+
+def test_should_return_path_of_testdir():
+    path = get_module_paths(["mypy_upgrade"])[0]
+    assert path == pathlib.Path(__file__, "../../src/mypy_upgrade").resolve()
