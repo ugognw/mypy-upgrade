3cef76c6791da41bcf6c693be49abdcb69f84bd0
Ugochukwu Nwosu <ugognw@gmail.com>
2023-07-31 01:38:09 -0600

    Change test module to look up
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- tests/test_get_module_paths.py
+++ tests/test_get_module_paths.py
@@ -4,8 +4,8 @@ from mypy_upgrade.cli import get_module_paths
 
 
 def test_should_return_path_of_testfile():
-    path = get_module_paths([__name__])[0]
-    assert path == pathlib.Path(__file__).resolve()
+    path = get_module_paths(["mypy_upgrade.cli"])[0]
+    assert path == pathlib.Path("src/mypy_upgrade/cli.py").resolve()
 
 
 def test_should_return_path_of_testdir():
