7429de20620d043645147e381509511a84c0d4b9
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-02 15:06:13 -0600

    Fix bug with multiline statements
    Tokenizing a single line of a multiline statement results in an error
    preventing the extraction of the Python code and comment. A temporary
    workaround is to catch this error and find the comment using string methods.
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- src/mypy_upgrade/utils.py
+++ src/mypy_upgrade/utils.py
@@ -9,16 +9,24 @@ def split_code_and_comment(line: str) -> tuple[str, str]:
     """Split a line of code into the code part and comment part."""
     reader = io.StringIO(line).readline
 
-    comment_tokens = [
-        t
-        for t in tokenize.generate_tokens(reader)
-        if t.type == tokenize.COMMENT
-    ]
+    try:
+        comment_tokens = [
+            t
+            for t in tokenize.generate_tokens(reader)
+            if t.type == tokenize.COMMENT
+        ]
+        if not comment_tokens:
+            return line, ""
 
-    if not comment_tokens:
-        return line, ""
+        comment_token = comment_tokens[0]
+        python_code = line[0 : comment_token.start[1]]
+        python_comment = line[comment_token.start[1] :]
+    except tokenize.TokenError:  # compromise for multi-line statements
+        comment_start = line.find("#")
+        if comment_start >= 0:
+            python_code = line[0:comment_start]
+            python_comment = line[comment_start:]
+        else:
+            python_code, python_comment = (line, "")
 
-    comment_token = comment_tokens[0]
-    python_code = line[0 : comment_token.start[1]]
-    python_comment = line[comment_token.start[1] :]
     return python_code, python_comment
