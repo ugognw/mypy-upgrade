59e8a8d19919b724de574adb12ecd660f2c38278
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-03 01:53:08 -0600

    redefine files
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,16 +9,16 @@ from mypy_upgrade.parsing import MypyError, parse_mypy_report
 @pytest.fixture(
     name="report",
     params=(
-        "mypy_report_6269340a3.txt",
-        "mypy_report_6269340a3_black.txt",
-        "mypy_report_35af5282d.txt",
-        "mypy_report_35af5282d_black.txt",
+        "35af5282d/baseline_report_47a422c16.txt",
+        "6269340a3/baseline_report_7c2def18.txt",
+        "6269340a3/second_report_96c979674.txt",
+        "6269340a3/third_report_ba79c42c7.txt",
     ),
 )
 def fixture_report(
     shared_datadir: pathlib.Path, request: pytest.FixtureRequest
 ) -> typing.TextIO:
-    file = shared_datadir / "strict_mypy_reports" / request.param
+    file = shared_datadir / request.param
     with pathlib.Path(file).open(encoding="utf-8") as report:
         yield report
 
