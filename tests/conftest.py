fbf8fa9ffd62209c9768f23cc938523bd5ca4b2a
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-05 12:46:24 -0600

    Update mypy typechecking report file names
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,11 +12,13 @@ from mypy_upgrade.parsing import MypyError, parse_mypy_report
 @pytest.fixture(
     name="report",
     params=(
-        "35af5282d/baseline_report_47a422c16.txt",
-        "35af5282d/second_report_6f100101a.txt",
-        "6269340a3/baseline_report_7c2def18.txt",
-        "6269340a3/second_report_96c979674.txt",
-        "6269340a3/third_report_ba79c42c7.txt",
+        "35af5282d/with_columns/baseline_report_47a422c16.txt",
+        "35af5282d/without_columns/baseline_report_47a422c16.txt",
+        "35af5282d/without_columns/second_report_6f100101a.txt",
+        "6269340a3/with_columns/baseline_report_7c2def18.txt",
+        "6269340a3/without_columns/baseline_report_7c2def18.txt",
+        "6269340a3/without_columns/second_report_96c979674.txt",
+        "6269340a3/without_columns/third_report_ba79c42c7.txt",
     ),
 )
 def fixture_report(
