64c678a62733401add6487eeb31267411389d272
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-21 21:40:36 -0600

    Update `silence_errors_in_report`
    The functionality of `_collect_errors` can be encapsulated in four lines and
    does not warrant a separate function. Further, it may eventually be decided to
    change the signature of `silence_errors_in_report` to accept a `TextIO`
    instance and perform any file opening upstream.

--- src/mypy_upgrade/silence.py
+++ src/mypy_upgrade/silence.py
@@ -6,7 +6,8 @@ import itertools
 import pathlib
 import sys
 import tokenize
-from collections.abc import Iterable, Iterator
+from collections.abc import Iterable
+from operator import attrgetter
 from typing import NamedTuple, TextIO
 
 if sys.version_info < (3, 8):
@@ -17,17 +18,14 @@ else:
 from mypy_upgrade.editing import (
     add_type_ignore_comment,
     format_type_ignore_comment,
-    remove_unused_type_ignore_comment,
 )
-from mypy_upgrade.filter import filter_mypy_errors
+from mypy_upgrade.filter import filter_by_source
 from mypy_upgrade.parsing import (
     MypyError,
     parse_mypy_report,
     string_to_error_codes,
 )
 from mypy_upgrade.utils import (
-    get_lines_and_tokens,
-    get_safe_to_silence_errors,
     split_into_code_and_comment,
 )
 from mypy_upgrade.warnings import MISSING_ERROR_CODES, TRY_SHOW_ABSOLUTE_PATH
@@ -48,14 +46,6 @@ class MypyUpgradeResult(NamedTuple):
     silenced: tuple[MypyError, ...]
     not_silenced: tuple[MypyError, ...]
     messages: tuple[str, ...]
-
-
-def _collect_errors(report: pathlib.Path | None) -> list[MypyError]:
-    """Parse errors from provided file or standard input."""
-    if report is not None:
-        with pathlib.Path(report).open(encoding="utf-8") as file:
-            return parse_mypy_report(file)
-    return parse_mypy_report(sys.stdin)
 
 
 def _extract_error_details(
@@ -233,14 +223,18 @@ def silence_errors_in_report(
         which an error is to be silenced or `mypy-upgrade`-related warnings
         are raised during execution are stored in the `messages` attribute.
     """
-    errors = _collect_errors(report)
-    filtered_errors = filter_mypy_errors(
+    if report is None:
+        errors = parse_mypy_report(sys.stdin)
+    else:
+        with report.open(encoding="utf-8") as file:
+            errors = parse_mypy_report(file)
+    source_filtered_errors = filter_by_source(
         errors=errors, packages=packages, modules=modules, files=files
     )
     messages = []
     silenced: list[MypyError] = []
     for filename, filename_grouped_errors in itertools.groupby(
-        errors, key=lambda error: error.filename
+        errors, key=attrgetter("filename")
     ):
         try:
             with pathlib.Path(filename).open(
@@ -252,14 +246,14 @@ def silence_errors_in_report(
                     description_style,
                     fix_me,
                 )
-            silenced.append(safe_to_silence)
+            silenced.extend(safe_to_silence)
         except FileNotFoundError:
             messages += TRY_SHOW_ABSOLUTE_PATH.replace("{filename}", filename)
         except tokenize.TokenError:
             messages += f"Unable to tokenize file: {filename}"
 
-    if any(error.error_code is None for error in filtered_errors):
+    if any(error.error_code is None for error in source_filtered_errors):
         messages += MISSING_ERROR_CODES
 
-    not_silenced = [e for e in filtered_errors if e not in silenced]
+    not_silenced = [e for e in source_filtered_errors if e not in silenced]
     return MypyUpgradeResult((*silenced,), (*not_silenced,), (*messages,))
