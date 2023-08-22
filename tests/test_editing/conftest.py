0d6df17102670eb1898247b75d98e93d675f0108
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-22 02:13:29 -0600

    Move common fixtures to conftest.py
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

---
+++ tests/test_editing/conftest.py
@@ -0,0 +1,52 @@
+from itertools import combinations
+
+import pytest
+
+ERROR_CODES = [
+    "",
+    "override",
+    "arg-type",
+    "attr-defined",
+]
+
+CODE_COMBINATIONS = (
+    [[e] for e in ERROR_CODES]
+    + list(combinations(ERROR_CODES[1:], r=2))
+    + [ERROR_CODES[1:]]
+)
+
+
+COMMENT_SUFFIXES = [
+    "",
+    "# noqa",
+]
+
+
+@pytest.fixture(name="error_codes", scope="class", params=CODE_COMBINATIONS)
+def fixture_error_codes(request: pytest.FixtureRequest) -> str:
+    comment_suffix: str = request.param
+    return comment_suffix
+
+
+@pytest.fixture(
+    name="codes_to_remove", scope="class", params=CODE_COMBINATIONS
+)
+def fixture_codes_to_remove(request: pytest.FixtureRequest) -> str:
+    comment_suffix: str = request.param
+    return comment_suffix
+
+
+@pytest.fixture(name="stub", scope="class")
+def fixture_stub(error_codes: list[str]) -> str:
+    return f'# type: ignore[{", ".join(error_codes)}]'
+
+
+@pytest.fixture(name="comment_suffix", scope="class", params=COMMENT_SUFFIXES)
+def fixture_comment_suffix(request: pytest.FixtureRequest) -> str:
+    comment_suffix: str = request.param
+    return comment_suffix
+
+
+@pytest.fixture(name="comment", scope="class")
+def fixture_comment(stub: str, comment_suffix: str) -> str:
+    return f"{stub} {comment_suffix}"
--- tests/test_editing/test_remove_unused_type_ignore_comment.py
+++ tests/test_editing/test_remove_unused_type_ignore_comment.py
@@ -2,67 +2,13 @@ from __future__ import annotations
 
 import re
 from collections.abc import Iterable
-from itertools import combinations
 
 import pytest
 
 from mypy_upgrade.editing import remove_unused_type_ignore_comments
 
-ERROR_CODES = [
-    "",
-    "override",
-    "arg-type",
-    "attr-defined",
-]
-
-CODE_COMBINATIONS = (
-    [[e] for e in ERROR_CODES]
-    + list(combinations(ERROR_CODES[1:], r=2))
-    + [ERROR_CODES[1:]]
-)
-
-
-COMMENT_SUFFIXES = [
-    "",
-    "# noqa",
-]
-
 
 class TestAllCombinations:
-    @staticmethod
-    @pytest.fixture(
-        name="error_codes", scope="class", params=CODE_COMBINATIONS
-    )
-    def fixture_error_codes(request: pytest.FixtureRequest) -> str:
-        comment_suffix: str = request.param
-        return comment_suffix
-
-    @staticmethod
-    @pytest.fixture(
-        name="codes_to_remove", scope="class", params=CODE_COMBINATIONS
-    )
-    def fixture_codes_to_remove(request: pytest.FixtureRequest) -> str:
-        comment_suffix: str = request.param
-        return comment_suffix
-
-    @staticmethod
-    @pytest.fixture(name="stub", scope="class")
-    def fixture_stub(error_codes: list[str]) -> str:
-        return f'# type: ignore[{", ".join(error_codes)}]'
-
-    @staticmethod
-    @pytest.fixture(
-        name="comment_suffix", scope="class", params=COMMENT_SUFFIXES
-    )
-    def fixture_comment_suffix(request: pytest.FixtureRequest) -> str:
-        comment_suffix: str = request.param
-        return comment_suffix
-
-    @staticmethod
-    @pytest.fixture(name="comment", scope="class")
-    def fixture_comment(stub: str, comment_suffix: str) -> str:
-        return f"{stub} {comment_suffix}"
-
     @staticmethod
     @pytest.fixture(name="remove_result", scope="class")
     def fixture_remove_result(comment: str, codes_to_remove: list[str]) -> str:
