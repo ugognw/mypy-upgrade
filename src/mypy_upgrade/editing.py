183bdbd2afbee9b23f9192c05ef1c2ee102868ff
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-22 03:00:18 -0600

    Refactor `editing`
    Using `string_to_error_codes` simplifies the code.
    
    Using sets offloads checking uniqueness.
    
    No formatting should be done in the comment.
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- src/mypy_upgrade/editing.py
+++ src/mypy_upgrade/editing.py
@@ -5,55 +5,34 @@ from __future__ import annotations
 import re
 from collections.abc import Collection
 
+from mypy_upgrade.parsing import string_to_error_codes
+
 
 def add_type_ignore_comment(comment: str, error_codes: list[str]) -> str:
     """Add a `type: ignore` comment with error codes to in-line comment.
 
     Args:
-        comment: a string representing a comment in which to add a type ignore
-            comment.
+        comment: a string representing a comment in which to add a `type:
+            ignore` comment.
         error_codes: the error codes to add to the `type: ignore` comment.
 
     Returns:
         A copy of the original comment with a `type: ignore[error-code]`
         comment added
     """
-    old_type_ignore_re = re.compile(
-        r"type\s*:\s*ignore(\[(?P<error_code>[a-z, \-]+)\])?"
-    )
-
-    # Handle existing "type: ignore" comments
-    match = old_type_ignore_re.search(comment)
-    if match:
-        if match.group("error_code"):
-            old_error_codes = set(
-                match.group("error_code").replace(" ", "").split(",")
-            )
-            error_codes.extend(
-                e for e in old_error_codes if e not in error_codes
-            )
-        comment = old_type_ignore_re.sub("", comment)
-
-        # Check for other comments; otherwise, remove comment
-        if not re.search(r"[^#\s]", comment):
-            comment = ""
-        else:
-            comment = f' # {comment.lstrip("# ")}'
-    elif comment:
-        # format comment
-        comment = f' # {comment.lstrip("# ")}'
-
-    sorted_error_codes = ", ".join(sorted(error_codes))
-
-    return f"# type: ignore[{sorted_error_codes}]{comment}"
+    type_ignore = re.compile(r"# type\s*:\s*ignore(\[[a-z, \-]*\])?")
+    existing_codes = string_to_error_codes(type_ignore.match(comment).string)
+    error_codes.extend(existing_codes)
+    codes = f'[{", ".join(sorted({*error_codes}))}]' if error_codes else ""
+    return type_ignore.sub(f"# type: ignore{codes}", comment).rstrip()
 
 
 def format_type_ignore_comment(comment: str) -> str:
     """Remove excess whitespace and commas from a `"type: ignore"` comment."""
-    type_ignore_re = re.compile(
+    type_ignore = re.compile(
         r"type\s*:\s*ignore(\[(?P<error_codes>[a-z, \-]*)\])?"
     )
-    match = type_ignore_re.search(comment)
+    match = type_ignore.search(comment)
 
     # Format existing error codes
     if match is None:
@@ -64,7 +43,7 @@ def format_type_ignore_comment(comment: str) -> str:
     error_codes = [e for e in comma_separated_codes.split(",") if e]
 
     codes = f'[{", ".join(error_codes)}]' if error_codes else ""
-    return type_ignore_re.sub(f"type: ignore{codes}", comment).rstrip()
+    return type_ignore.sub(f"type: ignore{codes}", comment, count=1).rstrip()
 
 
 def remove_unused_type_ignore_comments(
@@ -80,15 +59,15 @@ def remove_unused_type_ignore_comments(
     Returns:
         A copy of the original string with the specified error codes removed.
     """
-    type_ignore_re = re.compile(
+    type_ignore = re.compile(
         r"type\s*:\s*ignore(\[(?P<error_code>[a-z, \-]+)\])?"
     )
     if "*" in codes_to_remove:
-        return type_ignore_re.sub("", comment)
+        return type_ignore.sub("", comment)
 
-    match = type_ignore_re.search(comment)
+    match = type_ignore.search(comment)
     old_codes = match.group("error_code") or ""
     new_codes = old_codes
     for code in codes_to_remove:
         new_codes = new_codes.replace(code, "")
-    return type_ignore_re.sub(f"type: ignore[{new_codes}]", comment)
+    return type_ignore.sub(f"type: ignore[{new_codes}]", comment, count=1)
--- tests/test_editing/test_add_type_ignore_comment.py
+++ tests/test_editing/test_add_type_ignore_comment.py
@@ -4,14 +4,6 @@ import pytest
 
 from mypy_upgrade.editing import add_type_ignore_comment
 
-TYPE_IGNORE_STUBS = [
-    "",
-    "type: ignore[<error-code>]",
-    "type: ignore[<error-code>, <error-code>]",
-]
-
-COMMENT_SUFFIXES = ["", "# noqa", " comment"]
-
 ERROR_CODES = [
     "arg-type",
     "attr-defined",
@@ -20,74 +12,51 @@ ERROR_CODES = [
 ]
 
 
-@pytest.fixture(name="type_ignore_stub", params=TYPE_IGNORE_STUBS)
-def fixture_type_ignore_stub(request: pytest.FixtureRequest) -> str:
-    type_ignore_stub: str = request.param
-    return type_ignore_stub
+class TestAllCombinations:
+    @staticmethod
+    @pytest.fixture(name="final_comment", scope="class")
+    def fixture_final_comment(comment: str) -> str:
+        type_ignore_comment: str = add_type_ignore_comment(
+            comment, ERROR_CODES
+        )
+        return type_ignore_comment
 
+    @staticmethod
+    def test_should_retain_existing_error_codes(
+        final_comment: str,
+        error_codes: list[str],
+    ) -> None:
+        assert all(e in final_comment for e in error_codes)
 
-@pytest.fixture(name="comment_suffix", params=COMMENT_SUFFIXES)
-def fixture_comment_suffix(request: pytest.FixtureRequest) -> str:
-    comment_suffix: str = request.param
-    return comment_suffix
+    @staticmethod
+    def test_should_add_new_error_codes(final_comment: str) -> None:
+        assert all(code in final_comment for code in ERROR_CODES)
 
+    @staticmethod
+    def test_should_preserve_existing_comment(
+        final_comment: str, comment_suffix: str
+    ) -> None:
+        assert comment_suffix.lstrip("# ") in final_comment
 
-@pytest.fixture(name="initial_error_codes")
-def fixture_initial_error_codes(type_ignore_stub: str) -> list[str]:
-    num_of_initial_error_codes = type_ignore_stub.count("<error-code>")
-    return ERROR_CODES[:num_of_initial_error_codes]
+    @staticmethod
+    def test_should_place_type_ignore_comment_first(
+        final_comment: str,
+    ) -> None:
+        assert final_comment.startswith("# type: ignore")
 
+    @staticmethod
+    def test_should_sort_error_codes(
+        final_comment: str, error_codes: list[str]
+    ) -> None:
+        sorted_error_codes = sorted(set(ERROR_CODES).union(error_codes))
+        indices = [
+            final_comment.index(error_code)
+            for error_code in sorted_error_codes
+        ]
+        assert indices == sorted(indices)
 
-@pytest.fixture(name="type_ignore_comment")
-def fixture_type_ignore_comment(
-    type_ignore_stub: str, initial_error_codes: str
-) -> str:
-    type_ignore_comment = type_ignore_stub
-    for code in initial_error_codes:
-        type_ignore_comment = type_ignore_stub.replace("<error-code>", code, 1)
-
-    return type_ignore_comment
-
-
-@pytest.fixture(name="comment")
-def fixture_comment(type_ignore_comment: str, comment_suffix: str) -> str:
-    return type_ignore_comment + comment_suffix
-
-
-@pytest.fixture(name="final_comment")
-def fixture_final_comment(comment: str) -> str:
-    type_ignore_comment: str = add_type_ignore_comment(comment, ERROR_CODES)
-    return type_ignore_comment
-
-
-def test_should_retain_existing_error_codes(
-    final_comment: str,
-    initial_error_codes: list[str],
-) -> None:
-    assert all(e in final_comment for e in initial_error_codes)
-
-
-def test_should_add_new_error_codes(final_comment: str) -> None:
-    assert all(code in final_comment for code in ERROR_CODES)
-
-
-def test_should_preserve_existing_comment(
-    final_comment: str, comment_suffix: str
-) -> None:
-    assert comment_suffix.lstrip("# ") in final_comment
-
-
-def test_should_place_type_ignore_comment_first(final_comment: str) -> None:
-    assert final_comment.startswith("# type: ignore")
-
-
-def test_should_sort_error_codes(final_comment: str) -> None:
-    sorted_error_codes = sorted(ERROR_CODES)
-    indices = [
-        final_comment.index(error_code) for error_code in sorted_error_codes
-    ]
-    assert indices == sorted(indices)
-
-
-def test_should_not_add_duplicate_error_codes(final_comment: str) -> None:
-    assert all(final_comment.count(error_code) for error_code in ERROR_CODES)
+    @staticmethod
+    def test_should_not_add_duplicate_error_codes(final_comment: str) -> None:
+        assert all(
+            final_comment.count(error_code) for error_code in ERROR_CODES
+        )
