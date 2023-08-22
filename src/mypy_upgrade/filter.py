f9a7fa1e9ed49e5a587ba4dc2222b9cf86375354
Ugochukwu Nwosu <ugognw@gmail.com>
2023-08-21 20:47:56 -0600

    Move `get_safe_to_silence_errors` and refactor
    `get_safe_to_silence_errors` and, by extension, `UnsilenceableRegion`,
    `find_unsilenceable_regions`, `find_unsilenceable_regions`,
    `find_safe_end_line`, and `get_safe_to_silence_errors` are all related to
    filtering `MypyError`s, so it is logical to move them to the `filter` module.
    
    Only the line numbers of both the `start` and `end` attributes of
    `UnsilenceableRegion`s are used, so implementing these attributes as integers
    instead of tuples simplifies the tuple.
    
    `_find_unsilenceable_regions` is only used by `get_safe_to_silence_errors`, so
    it makes sense to make it private to the module in which it is defined.
    Accepting `comments` as a sequence of strings instead of `TokenInfo` instances
    enables the reuse of a list of comment strings, which can be constructed in
    steps upstream of `get_safe_to_silence_errors`. It also simplifies the
    second conditional check within the for loop.
    
    Refactoring `find_safe_end_line` to return a boolean (instead of an integer)
    simplifies use of its return value, since all that it is used for is to
    determine if an error can be silenced. (This is brought about by the removal
    of the capability to silence errors on the first line of, but still preceding,
    multiline strings.) Renaming `find_safe_end_line` to `_is_safe_to_silence`
    better reflects its function and the fact that it is used only by
    `get_safe_to_silence_errors`.
    
    Renaming `get_safe_to_silence_errors` to `filter_by_silenceability` better
    reflects its function.
    
    The use of keyword-only arguments is intended to improve maintainability in
    the event of adding/reordering parameters.
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- src/mypy_upgrade/filter.py
+++ src/mypy_upgrade/filter.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 
 import importlib.abc
 import pathlib
+import tokenize
+from collections.abc import Iterable, Sequence
 from importlib import util
+from typing import NamedTuple
 
 from mypy_upgrade.parsing import MypyError
 
@@ -83,3 +86,119 @@ def filter_by_source(
             selected.append(error)
 
     return selected
+
+
+class UnsilenceableRegion(NamedTuple):
+    """A region within a source code that cannot be silenced by an inline
+    comment.
+
+    Attributes:
+        start: an integer representing the start line of the unsilenceable
+            region (1-indexed).
+        end: an integer representing the end line of the unsilenceable
+            region (1-indexed).
+
+        When start = end, it is interpreted that the Unsilenceable
+        region is an explicitly continued line.
+    """
+
+    start: int
+    end: int
+
+
+def _find_unsilenceable_regions(
+    *,
+    tokens: Iterable[tokenize.TokenInfo],
+    comments: Sequence[str],
+) -> list[UnsilenceableRegion]:
+    """Find the regions encapsulated by line continuation characters or
+    by multiline strings.
+
+    Args:
+        tokens: an iterable containing `TokenInfo` objects.
+        comments: a sequence of strings representing code comments-one for
+            each line in the source from which `tokens` is generated.
+
+    Returns:
+        A list of `UnsilenceableRegion` objects.
+
+        Multiline strings are represented by `UnsilenceableRegion` objects
+        whose first entries in their `start` and `end` attributes differ.
+        Explicitly continued lines are represented by `UnsilenceableRegion`
+        objects whose first entries in their `start` and `end` attributes are
+        the same.
+    """
+    unsilenceable_regions: set[UnsilenceableRegion] = {}
+    for token in tokens:
+        if (
+            token.start[0] != token.end[0]
+            and token.exact_type == tokenize.STRING
+        ):
+            region = UnsilenceableRegion(token.start[0], token.end[0])
+            unsilenceable_regions.add(region)
+        elif (
+            token.line.rstrip("\r\n").endswith("\\")
+            and not comments[token.end[0] - 1]
+        ):
+            region = UnsilenceableRegion(token.end[0], token.end[0])
+            unsilenceable_regions.add(region)
+
+    return list(unsilenceable_regions)
+
+
+def _is_safe_to_silence(
+    *, error: MypyError, unsilenceable_regions: Iterable[UnsilenceableRegion]
+) -> bool:
+    """Determine if the error is safe to silence
+
+    Args:
+        error: a `MypyError` for which a type error suppression comment is to
+            placed.
+        unsilenceable_regions: an iterable of `UnsilenceableRegion`s.
+
+    Returns:
+        `False` if the error is in an `UnsilenceableRegion` or its error code
+        is "syntax"; `True` otherwise.
+    """
+    if error.error_code == "syntax":
+        return False
+
+    for region in unsilenceable_regions:
+        # Error within an UnsilenceableRegion (but not last line of multiline
+        # string)
+        if region.start <= error.line_no <= region.end and not (
+            error.line_no == region.end and region.start != region.end
+        ):
+            return False
+
+    return True
+
+
+def filter_by_silenceability(
+    *,
+    errors: Iterable[MypyError],
+    comments: Sequence[str],
+    tokens: list[tokenize.TokenInfo],
+) -> list[MypyError]:
+    """Filter `MypyError`s by those which are safe to silence.
+
+    Args:
+        errors: the errors whose line numbers are to be corrected.
+        comments: a container of strings representing code comments.
+        tokens: an iterable containing `TokenInfo` objects.
+
+    Returns:
+        A list in which each entry is a `MypyError` from `errors` that can be
+        silenced with a type suppression comment.
+    """
+    unsilenceable_regions = _find_unsilenceable_regions(
+        tokens=tokens, comments=comments
+    )
+    safe_to_silence = []
+    for error in errors:
+        if _is_safe_to_silence(
+            error=error, unsilenceable_regions=unsilenceable_regions
+        ):
+            safe_to_silence.append(error)
+
+    return safe_to_silence
--- src/mypy_upgrade/utils.py
+++ src/mypy_upgrade/utils.py
@@ -4,30 +4,12 @@ from __future__ import annotations
 
 import io
 import tokenize
-from collections.abc import Iterable, Sequence
 from typing import NamedTuple, TextIO
 
-from mypy_upgrade.parsing import MypyError
 
-
-class UnsilenceableRegion(NamedTuple):
-    """A region within a source code that cannot be silenced by an inline
-    comment.
-
-    Attributes:
-        start: a 2-tuple representing the start of the unsilenceable region
-            whose first entry is the start line (1-indexed) and whose second
-            entry is the start column offset.
-        end: a 2-tuple representing the end of the unsilenceable region
-            whose first entry is the end line (1-indexed) and whose second
-            entry is the end column offset.
-
-        When start[0] = end[0], it is interpreted that the Unsilenceable
-        region is an explicitly continued line.
-    """
-
-    start: tuple[int, int]  # line, column
-    end: tuple[int, int]  # line, column
+class CommentSplitLine(NamedTuple):
+    code: str
+    comment: str
 
 
 def split_into_code_and_comment(line, comments) -> tuple[str, str]:
@@ -61,108 +43,3 @@ def get_lines_and_tokens(
         tokens.append(token)
 
     return lines, tokens
-
-
-def find_unsilenceable_regions(
-    tokens: Iterable[tokenize.TokenInfo],
-    comments: Sequence[tokenize.TokenInfo],
-) -> list[UnsilenceableRegion]:
-    """Find the regions encapsulated by line continuation characters or
-    by multiline strings
-
-    Args:
-        tokens: an `Iterable` of `tokenize.TokenInfo` instances.
-        comments: an `Iterable` of `tokenize.TokenInfo` instances which are
-            `tokenize.COMMENT`'s.
-
-    Returns:
-        A list of UnsilenceableRegion objects.
-
-        Multiline strings are represented by UnsilienceableRegion objects
-        whose first entries in their `start` and `end` attributes are
-        different. Explicitly continued lines are represented by
-        UnsilienceableRegion objects whose first entries in their `start` and
-        `end` attributes are the same.
-    """
-    unsilenceable_regions = []
-    for token in tokens:
-        if (
-            token.start[0] != token.end[0]
-            and token.exact_type == tokenize.STRING
-        ):
-            region = UnsilenceableRegion(token.start, token.end)
-            unsilenceable_regions.append(region)
-        elif token.line.rstrip("\r\n").endswith("\\") and not any(
-            comment.line == token.line for comment in comments
-        ):
-            start = token.end[0], 0
-            end = token.end[0], len(token.line)
-            region = UnsilenceableRegion(start, end)
-            unsilenceable_regions.append(region)
-
-    return unsilenceable_regions
-
-
-def find_safe_end_line(
-    error: MypyError, unsilenceable_regions: Iterable[UnsilenceableRegion]
-) -> int:
-    """Find a syntax-safe line on which to place an error suppression comment
-    for the given error.
-
-    Args:
-        error: a `MypyError` for which a type error suppression comment is to
-            placed.
-        unsilenceable_regions: an `Iterable` in which each entry is an
-            `UnsilenceableRegion`.
-
-    Returns:
-        An integer representing a safe line on which to place an error
-        suppression comment if it exists. If no safe line exists, this method
-        returns -1.
-    """
-    for region in unsilenceable_regions:
-        # It is safe to comment the last line of a multiline string
-        if error.line_no == region.end[0] and region.start[0] != region.end[0]:
-            continue
-
-        # Error within an UnsilenceableRegion (but not last line of multiline
-        # string)
-        if region.start[0] <= error.line_no <= region.end[0]:
-            return -1
-
-    return error.line_no
-
-
-def get_safe_to_silence_errors(
-    tokens: list[tokenize.TokenInfo],
-    comments: list[tokenize.TokenInfo],
-    errors: Iterable[MypyError],
-) -> list[MypyError]:
-    """Find the MypyErrors which are safe to silence.
-
-    Args:
-        unsilenceable_regions: an iterable whose elements are
-            `UnsilenceableRegion`'s in the same file as the errors.
-        errors: the errors whose line numbers are to be corrected.
-
-    Returns:
-        A list in which each entry is a `MypyError` from `errors` for which
-        type suppression comments can be added (with line numbers corrected).
-    """
-    unsilenceable_regions = find_unsilenceable_regions(tokens, comments)
-    safe_to_silence = []
-    for error in errors:
-        end_line = find_safe_end_line(error, unsilenceable_regions)
-
-        if end_line != -1 and error.error_code != "syntax":
-            safe_to_silence.append(
-                MypyError(
-                    error.filename,
-                    end_line,
-                    error.col_offset,
-                    error.message,
-                    error.error_code,
-                )
-            )
-
-    return safe_to_silence
